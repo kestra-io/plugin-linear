c982d1132bf1855681ba97db864ac140a222ca99
Malay Dewangan <malaydewangan310@gmail.com>
2025-11-07 16:07:58 +0530

    doc: add missing package-info.java

---
+++ src/main/java/io/kestra/plugin/linear/issues/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for managing issues using Linear API.",
+    categories = PluginSubGroup.PluginCategory.OTHER
+)
+package io.kestra.plugin.linear.issues;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
