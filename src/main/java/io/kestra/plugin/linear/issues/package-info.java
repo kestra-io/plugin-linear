7ba47d17ad61d7fa9b3fa3afa4e0827cb40ca88c
ben8t <pimpaudben@gmail.com>
2025-12-30 15:39:20 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/linear/issues/package-info.java
+++ src/main/java/io/kestra/plugin/linear/issues/package-info.java
@@ -1,6 +1,9 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for managing issues using Linear API.",
-    categories = PluginSubGroup.PluginCategory.OTHER
+    categories = PluginSubGroup.PluginCategory.OTHER,
+    categories = {
+        PluginSubGroup.PluginCategory.BUSINESS
+    }
 )
 package io.kestra.plugin.linear.issues;
 
